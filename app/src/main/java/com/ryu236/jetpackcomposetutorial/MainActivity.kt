ad88f624064acd7a480bcd2bfec2b7bc1ab62697
Ryu236 <ryu.adv.2360@gmail.com>
2019-11-09 18:14:07 +0900

    :sparkles: Feat: add layouts and a header image

--- app/src/main/java/com/ryu236/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/ryu236/jetpackcomposetutorial/MainActivity.kt
@@ -3,31 +3,43 @@ package com.ryu236.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.unaryPlus
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.setContent
-import androidx.ui.material.MaterialTheme
+import androidx.ui.foundation.DrawImage
+import androidx.ui.layout.*
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Greeting("Android")
-            }
+            NewsStory()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewsStory() {
+    val img = +imageResource(R.drawable.header)
+    Column(
+        crossAxisSize = LayoutSize.Expand,
+        modifier = Spacing(16.dp)
+    ) {
+        Container(expanded = true, height = 180.dp) {
+            DrawImage(img)
+        }
+        HeightSpacer(16.dp)
+        Text("A day in Shark Fin Cove")
+        Text("Davenport California")
+        Text("December 2018")
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
-    }
+    NewsStory()
 }
